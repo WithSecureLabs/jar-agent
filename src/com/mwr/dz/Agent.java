86dbd7d58b303e3fdd40997287a2b5602d5035d9
Daniel Bradberry <user.email>
2013-10-02 11:09:33 +0100

    remove a little lint related to storing the agent uid

--- src/com/mwr/dz/Agent.java
+++ src/com/mwr/dz/Agent.java
@@ -26,16 +26,12 @@ public class Agent {
 	private Client client;
 	private String uid;
 	
-	private String jar_path;
-	
 	public Agent(String host, int port) {
-		//this.jar_path = Agent.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-		this.jar_path = Agent.getJarPath();
-		Log.i("weasel", jar_path);
-		this.device_info = new DeviceInfo(this.getUID(),
+		this.device_info = new DeviceInfo(
+				this.getUID(),
 				android.os.Build.MANUFACTURER,
-				"unknown",
-				"unknown");
+				android.os.Build.MODEL,
+				android.os.Build.VERSION.RELEASE);
 		this.endpoint = new Endpoint(
 				1,
 				"drozer Server",
@@ -45,11 +41,6 @@ public class Agent {
 				"",
 				"",
 				"");
-	}
-	
-	private static String getJarPath(){
-		
-		return "";
 	}
 	
 	public static void main(String[] args) {
@@ -67,43 +58,44 @@ public class Agent {
 		return null;
 	}
 
-    private String loadUID(){
-        String uid = null;
+	private String loadUID(){
+		String uid = null;
 
-        	
-	    	File uid_file = new File("uid.txt");
-	    	BufferedReader reader;
-	        if(uid_file.exists()){
-	            try{
-	                reader = new BufferedReader(new InputStreamReader(new FileInputStream(uid_file)));
-	                uid = reader.readLine();
-	                reader.close();
-	            }catch(IOException e){
-	            }
-	        }
-        
-        return uid;
-    }
+		File uid_file = new File("uid.txt");
+		if(uid_file.exists()) {
+			try {
+				BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(uid_file)));
+				uid = reader.readLine();
+				reader.close();
+			}
+			catch(IOException e) { }
+		}
+
+		return uid;
+	}
     
-    private String createUID(){
-    	String uid = new BigInteger(64, new SecureRandom()).toString(32);
-    	
-    	File uid_file = new File("uid.txt");
-    	try{
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(uid_file)));
-            writer.append(uid);
-            writer.flush();
-            writer.close();
-        }catch(IOException e){
-        }
-        
-        return uid;
-    }
+	private String createUID(){
+		String uid = new BigInteger(64, new SecureRandom()).toString(32);
+
+		File uid_file = new File("uid.txt");
+		try {
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(uid_file)));
+			writer.append(uid);
+			writer.flush();
+			writer.close();
+		}
+		catch(IOException e) { }
+
+		return uid;
+	}
 
 	public String getUID() {
 		// we cannot request the ANDROID_ID, because we have no context, so
 		// we must generate one at random
+		
+		// first, check if there is a stored UID that we can load...
         this.uid = this.loadUID();
+        // ... if not then create one
 		if(this.uid == null)
 			this.uid = createUID();
 
